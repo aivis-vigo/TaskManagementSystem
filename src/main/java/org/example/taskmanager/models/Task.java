fa20ccd3bd3928493b7d9bc13e8682cd714efc4e
mfkrkfm <igor.zatserkl@gmail.com>
2026-02-11 11:31:10 +0200

    Switched to JPA and H2

--- src/main/java/org/example/taskmanager/models/Task.java
+++ src/main/java/org/example/taskmanager/models/Task.java
@@ -1,14 +1,26 @@
 package org.example.taskmanager.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "task_key")
     private String key;           // e.g. "PROJ-123"
     private String summary;       // e.g. "Fix login button not responding"
     private String description;
+    @Enumerated(EnumType.STRING)
     private TaskType type;          // BUG, TASK, STORY
+    @Enumerated(EnumType.STRING)
     private TaskStatus status;        // TODO, IN_PROGRESS, DONE
+    @Enumerated(EnumType.STRING)
     private TaskPriority priority;      // LOW, MEDIUM, HIGH, CRITICAL
     private String assignee;
+
+    public Task() {
+    }
 
     public Task(String key, String summary, TaskType type, TaskStatus status, TaskPriority priority, String assignee) {
         this.key = key;
@@ -35,12 +47,24 @@ public class Task {
         return type;
     }
 
+    public void setType(TaskType type) {
+        this.type = type;
+    }
+
     public TaskStatus getStatus() {
         return status;
     }
 
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
     public TaskPriority getPriority() {
         return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
     }
 
     public String getAssignee() { return assignee; }
